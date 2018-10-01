83dc23e16a9a7054570c74ae321b6f6aba6c791f
Zenno Bruinsma <zenno.bruinsma@bloomon.nl>
2018-10-01 21:17:50 +0200

    push

--- src/pages/Home/components/SearchBar/index.js
+++ src/pages/Home/components/SearchBar/index.js
@@ -1,5 +1,8 @@
 import React, { PureComponent } from 'react';
 import { Button } from 'reactstrap';
+
+import { TagsInput } from 'react-tagsinput';
+
 // import Helmet from 'react-helmet';
 
 // import { map } from 'lodash';
@@ -8,11 +11,23 @@ import './styles.scss';
 // const TITLES = ['Facturen', 'Wachtlijst'];
 
 export default class SearchBar extends PureComponent {
+  constructor() {
+    super();
+    this.state = { tags: [] };
+  }
+
+  handleChange(tags) {
+    this.setState({ tags });
+  }
+
   render() {
     return (
       <div>
-        <input value="helloooooooo" />
-        <Button>Rank</Button>
+        <div className="form">
+          <TagsInput value={this.state.tags} onChange={this.handleChange} />
+          <input className="form__input" placeholder="google.com" />
+          <Button className="form__button">rank</Button>
+        </div>
         <div className="search__tooltip">
           You can add multiple URL&#39;s by separating them with comma&#39;s
         </div>
