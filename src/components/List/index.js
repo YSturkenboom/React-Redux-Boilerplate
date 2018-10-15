b6e87edf6f01fb341cb2043b6a9b986b31a77408
Bart van den Aardweg <bartvandenaardweg@gmail.com>
2018-10-15 12:42:29 +0200

    Update build process, migrate moment to date-fns

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,8 +1,8 @@
-import Moment from 'react-moment';
 import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/pro-light-svg-icons';
+import { format } from 'date-fns';
 import './styles.scss';
 
 export default class ListsOverview extends PureComponent {
@@ -19,7 +19,7 @@ export default class ListsOverview extends PureComponent {
         </Link>
 
         <div className="list__date">
-          <Moment format="YYYY/MM/DD" date={createdAt} />
+          <time>{format(createdAt, 'DD/MM/YYYY')}</time>
         </div>
         <div className="list__name">{this.props.list.name}</div>
         <div className="list__amount">
