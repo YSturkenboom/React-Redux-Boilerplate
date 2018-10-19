3742aa7bb28aaa064e5fbea9d97cb14badd5bf7b
Yuri <yuri@storyofams.com>
2018-10-19 14:20:46 +0200

    Improved country code system, removed useless redirect

--- src/components/FloatingCircleButton/index.js
+++ src/components/FloatingCircleButton/index.js
@@ -1,29 +1,12 @@
 import React, { PureComponent } from 'react';
-import { Redirect } from 'react-router';
 import { Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/pro-regular-svg-icons';
 import './styles.scss';
 
 export default class FloatingCircleButton extends PureComponent {
-  constructor() {
-    super();
-    this.state = { redirect: false };
-  }
-
-  handleOnClick = () => {
-    this.setState({ redirect: true });
-    const { actionOnClick } = this.props;
-    actionOnClick();
-  };
-
   render() {
     const { addNewList } = this.props;
-    const { redirect } = this.state;
-
-    if (redirect) {
-      return <Redirect push to="/halloooo" />;
-    }
     return (
       <Button className="btn__floating-action" onClick={addNewList}>
         <FontAwesomeIcon icon={faPlus} />
