ece2757fe4958852fb00343e5fb463222c41dd8b
Yuri <yuri@storyofams.com>
2018-10-20 17:09:15 +0200

    Cleaned everything up

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import * as authActions from './auth';
-import * as siteRankActions from './siteRank';
-import * as registerActions from './register';
-import * as listActions from './lists';
-
-export { authActions, siteRankActions, registerActions, listActions };
